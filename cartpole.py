266230959fca40a5d8bb82e4e8f4b9aae4cced65
MSNR23 <144361856+MSNR23@users.noreply.github.com>
2024-04-10 11:59:28 +0900

    cartpole.py

--- cartpole.py
+++ cartpole.py
@@ -17,7 +17,6 @@ def bins(clip_min, clip_max, num):
 
 def digitize_state(observation):
 # 各値を４個の離散値に変換
-# np.digitizeは与えられた値をbinsで指定した基数に当てはめる関数。インデックスを返す。
     cart_pos, cart_v, pole_angle, pole_v = observation
     digitized = [np.digitize(cart_pos, bins=bins(-2.4, 2.4, 4)),
     np.digitize(cart_v, bins = bins(-3.0, 3.0, 4)),
@@ -25,14 +24,14 @@ def digitize_state(observation):
     np.digitize(pole_v, bins=bins(-2.0, 2.0, 4))]
 
 # 0~255に変換
-    return sum([x* (4**i) for i, x in enumerate(digitized)]) # インデックス付きループをすることができる。
+    return sum([x* (4**i) for i, x in enumerate(digitized)])
 
 
 def get_action(state, action, observation, reward, episode):
     next_state = digitize_state(observation)
 
     epsilon = 0.5 * (0.99 ** episode)
-    if epsilon <= np.random.uniform(0, 1): # もし0.2より一様乱数のほうが大きければ
+    if epsilon <= np.random.uniform(0, 1): # もしepsilonよりランダム値のほうが大きければ
         next_action = np.argmax(q_table[next_state])# q_tableの中で次に取りうる行動の中で最も価値の高いものを
 # next_actionに格納する
     else:# 逆に言うと20%の確率でランダムな行動を取る
